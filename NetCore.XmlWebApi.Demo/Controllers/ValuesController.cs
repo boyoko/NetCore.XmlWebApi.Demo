ce3bf13f65e88ab0ab6406288e25e7b85d8d3b1f
Administrator@TFS.LOCAL <Administrator@TFS.LOCAL>
2017-10-30 14:57:15 +0800

    first commit

---
+++ NetCore.XmlWebApi.Demo/Controllers/ValuesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCore.XmlWebApi.Demo.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace NetCore.XmlWebApi.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        [EnableCors("any")]
+        public IActionResult Get(int id)
+        {
+#region 构建返回值
+            var obj = new List<ExampleReturnObject>
+            {
+                new ExampleReturnObject
+                {
+                    Id = "001",
+                    Name ="jacky",
+                    Age = 20,
+                    Gender = "男"
+                },
+                new ExampleReturnObject
+                {
+                    Id = "002",
+                    Name ="xulei",
+                    Age = 40,
+                    Gender = "男"
+                },
+                new ExampleReturnObject
+                {
+                    Id = "003",
+                    Name ="asdf",
+                    Age = 35,
+                    Gender = "男"
+                }
+            };
+#endregion   
+            return Ok(obj);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NetCore.XmlWebApi.Demo/Models/ExampleReturnObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore.XmlWebApi.Demo.Models
+{
+    public class ExampleReturnObject
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
